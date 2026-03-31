4afbc591bf8e0413a5c8460fdd2648361475e13a
morain <morain.fu@gmail.com>
2026-03-31 17:14:01 +0800

    feat: update nvim config

--- private_dot_config/nvim/lua/plugins/flash.lua
+++ private_dot_config/nvim/lua/plugins/flash.lua
@@ -1,7 +1,7 @@
 return {
   'folke/flash.nvim',
   keys = {
-    { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
+    { 's', mode = { 'n', 'x' }, function() require('flash').jump() end, desc = 'Flash' },
     { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
     { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
     { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
